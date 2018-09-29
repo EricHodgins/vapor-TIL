385a96eca0aacd2887bf3f05041d23419c0f868c
Eric Hodgins <hodgins.e@gmail.com>
2018-09-29 13:25:39 -0400

    first commit

---
+++ Sources/App/Models/Acronym.swift
@@ -0,0 +1,30 @@
+
+import Vapor
+import FluentSQLite
+
+final class Acronym: Codable {
+    var id: Int? // This is a must
+    var short: String
+    var long: String
+    
+    init(short: String, long: String) {
+        self.short = short
+        self.long = long
+    }
+}
+
+//extension Acronym: Model {
+//    typealias Database = SQLiteDatabase
+//    typealias ID = Int
+//
+//    public static var idKey: IDKey = \Acronym.id
+//}
+
+// Improve the above Model
+extension Acronym: SQLiteModel {}
+
+// TO save the model you must create a table for it. Fluent does this with Migration.
+extension Acronym: Migration {}
+
+// Content - a wrapper around Codeable
+extension Acronym: Content {}
